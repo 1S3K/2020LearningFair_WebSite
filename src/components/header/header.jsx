bba88516253594ee1c930d1fdb54a9b6340d8b81
Taehyeon-Kim <taehyk528@gmail.com>
2020-11-17 22:37:20 +0900

    [feat] header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 
+import './header.css'; // header-web css
+
 class Header extends Component {
   render() {
     return (
-      <div>
-        
+    <header>
+      <div className="header-container">
+        <img src="/images/logo.png" alt="로고" className="header-logo"/>
+        <div className="header-profile">
+          <span className="header-profile-name">김재현님</span> 
+          <span className="header-profile-msg">환영합니다.</span>
+        </div>
       </div>
+    </header>
     );
   }
 }
