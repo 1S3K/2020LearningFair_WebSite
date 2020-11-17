cd19474a29b765dcffaad9e719a4db7fe385993d
Taehyeon-Kim <taehyk528@gmail.com>
2020-11-18 01:36:19 +0900

    [feat] contents page(no data)

--- src/components/contents/contents.jsx
+++ src/components/contents/contents.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
+
+import './contents.css';
+
+
+import Footer from '../footer/footer';
+import Header from '../header/header';
+import Navbar from '../navbar/navbar';
+import Article from '../article/article';
 
 class Contents extends Component {
+  state = {
+    isLogin: true,
+  }
+
   render() {
     return (
-      <div>
-        
-      </div>
+
+      <section className="contents-page">
+        <Header isLogin={this.state.isLogin} />
+        <section className="contents-page-middle">
+          <Navbar />
+          <Article />
+        </section>
+        <Footer />
+      </section>
+
     );
   }
 }
