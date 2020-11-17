8e473bfd2f888e728ae72e694661af5d4b728818
Taehyeon-Kim <taehyk528@gmail.com>
2020-11-17 23:24:40 +0900

    [fix] details

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './app.css';
+import './reset.css'
+
+
+import Login from './components/login/login';
 
 function App() {
-  return <h1>Hello :)</h1>;
+  return (
+    <Login />
+  )
 }
 
 export default App;
