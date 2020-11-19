961a6aac2a630ab011452ae487b3d4edf9220718
Taehyeon-Kim <taehyk528@gmail.com>
2020-11-19 12:28:08 +0900

    [feat] go home function

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 import './header.css'; // header-web css
 
@@ -8,11 +9,12 @@ class Header extends Component {
     // 로그인 상태에 따라 헤더의 프로필 표시
     const isLogin = this.props.isLogin;
     const name = sessionStorage.getItem("username");
+    const url = name ? '/main' : '/'
     
     return (
     <header style={{display: (isLogin ? '' : 'flex')}} className="header-container">
       {/* <div className="header-container"> */}
-        <img src="/images/logo.png" alt="로고" className="header-logo"/>
+        <Link to={url} style={{all: 'unset'}}><img src="/images/logo.png" alt="로고" className="header-logo"/></Link>
         {
           isLogin && 
           <div className="header-profile">
