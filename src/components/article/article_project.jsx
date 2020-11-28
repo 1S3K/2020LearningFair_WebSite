ef415f47b4e19b3a1934c4c7f66fd318fca1f1aa
Taehyeon-Kim <taehyk528@gmail.com>
2020-11-28 14:31:16 +0900

    [fix] 동영상 팝업 blur 처리 (black -> white)

--- src/components/article/article_project.jsx
+++ src/components/article/article_project.jsx
@@ -305,7 +305,7 @@ const ModalOverlay = styled.div`
   left: 0;
   bottom: 0;
   right: 0;
-  background-color: rgba(0, 0, 0, 0.6);
+  background-color: rgba(255, 255, 255, 0.6);
   z-index: 999;
 `
 
