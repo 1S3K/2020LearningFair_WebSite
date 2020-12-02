1e9a325ca353ab8265524b7ffb3b8dbe72f2003d
Ubuntu <ubuntu@ip-172-31-43-145.ap-northeast-2.compute.internal>
2020-12-02 03:50:27 +0000

    [FIX] IE 픽스  성공git add .git add .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import 'react-app-polyfill/stable';
-import 'react-app-polyfill/ie9'; // For IE 9-11 support
+import 'react-app-polyfill/ie11'; // For IE 9-11 support
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
