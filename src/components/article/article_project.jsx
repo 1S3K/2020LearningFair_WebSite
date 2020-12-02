96edcae8349974a9502be3a2666bd058537666fd
KangJunewoo <brianjune.dev@gmail.com>
2020-12-02 10:39:04 +0900

    [FIX]

--- src/components/article/article_project.jsx
+++ src/components/article/article_project.jsx
@@ -9,7 +9,7 @@ import ReactPlayer from 'react-player';
 import Modal from 'react-modal';
 import styled from 'styled-components';
 
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
 const customStyles = {
