2ed4d3d63b5a9a86bdbf0460e903226e1b4a8d97
KangJunewoo <brianjune.dev@gmail.com>
2020-12-02 12:22:55 +0900

    [FIX] ie호환 추가

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
-import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import 'react-app-polyfill/ie9'; // For IE 9-11 support
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 ReactDOM.render(
   <React.StrictMode>
